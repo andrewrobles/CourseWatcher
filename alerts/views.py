fa5f57d05ffd95477ccbb9720cf27f8b1c37c65d
andrewrobles <andrewrobles@berkeley.edu>
2018-11-23 08:33:29 -0800

    Exclamation mark

--- alerts/views.py
+++ alerts/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the alerts index.")
+    return HttpResponse("Hello, world! You're at the alerts index.")
