92c37d03157a0b42c99769b253f7036a255a53f0
andrewrobles <andrewrobles@berkeley.edu>
2018-11-22 23:28:17 -0800

    Switch to default database

--- courseWatcher/settings.py
+++ courseWatcher/settings.py
@@ -75,12 +75,8 @@ WSGI_APPLICATION = 'courseWatcher.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'coursewatcher',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
